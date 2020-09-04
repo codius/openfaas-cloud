91e23ea444756270774f9c2bdc6843eb56c335a1
Brandon Wilson <brandon@coil.com>
2020-09-04 10:13:44 -0500

    Web monetize system dashboard
    Use function specific payment pointers on function details page.
    Web-monetized visitors fund function balance when visiting function
    details page.
    Submit receipts to credit function balance.
    
    Signed-off-by: Brandon Wilson <brandon@coil.com>

--- dashboard/of-cloud-dashboard/handler.js
+++ dashboard/of-cloud-dashboard/handler.js
@@ -143,7 +143,7 @@ module.exports = async (event, context) => {
 }
 
 function replaceTokens(content, isSignedIn, claims) {
-    const { base_href, public_url, pretty_url, query_pretty_url, github_app_url, gitlab_url } = process.env;
+    const { base_href, public_url, pretty_url, query_pretty_url, github_app_url, gitlab_url, payment_pointer, receipts_url } = process.env;
     let replaced = content
 
     replaced = replaced.replace(/__BASE_HREF__/g, base_href);
@@ -154,6 +154,8 @@ function replaceTokens(content, isSignedIn, claims) {
     replaced = replaced.replace(/__ALL_CLAIMS__/g, claims);
     replaced = replaced.replace(/__GITHUB_APP_URL__/g, github_app_url || "");
     replaced = replaced.replace(/__GITLAB_URL__/g, gitlab_url || "");
+    replaced = replaced.replace(/__PAYMENT_POINTER__/g, payment_pointer);
+    replaced = replaced.replace(/__RECEIPTS_URL__/g, receipts_url);
 
     return replaced
 }
